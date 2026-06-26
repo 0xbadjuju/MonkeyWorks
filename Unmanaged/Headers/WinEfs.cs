48f75365ec2d7f3f3ab4d481c8b073ddabe10bf2
Alexander <1656007+0xbadjuju@users.noreply.github.com>
2026-06-26 10:43:04 -0400

    Add additional efs calls

---
+++ Unmanaged/Headers/WinEfs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MonkeyWorks.Unmanaged.Headers
+{
+    public sealed class WinEfs
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct EFS_RPC_BLOB
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public uint CbData;
+            public IntPtr BData;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ENCRYPTION_CERTIFICATE_HASH_LIST
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public uint NCertHash;
+            public IntPtr Users;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ENCRYPTION_CERTIFICATE_LIST
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public uint NUsers;
+            public IntPtr Users;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ENCRYPTED_FILE_METADATA_SIGNATURE
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public uint DwEfsAccessType;
+            public IntPtr CertificatesAdded;
+            public IntPtr EncryptionCertificate;
+            public IntPtr EfsStreamSignature;
+        }
+    }
+}
--- Unmanaged/Libraries/PInvoke/rpcrt4.cs
+++ Unmanaged/Libraries/PInvoke/rpcrt4.cs
@@ -1,28 +1,144 @@
 using MonkeyWorks.Unmanaged.Headers;
 using System;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace MonkeyWorks.Unmanaged.Libraries
 {
     public sealed class rpcrt4
     {
         [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcOpenFileRaw(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr hContext, IntPtr fileName, IntPtr flags);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcEncryptFileSrv(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcDecryptFileSrv(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr openFlag);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcQueryUsersOnFile(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr users);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcQueryRecoveryAgents(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr recoveryAgents);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcRemoveUsersFromFile(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr users);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcAddUsersToFile(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr encryptionCertificates);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcFileKeyInfo(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr infoClass, IntPtr keyInfo);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcDuplicateEncryptionInfoFile(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr srcFileName, IntPtr destFileName,
+            IntPtr dwCreationDisposition, IntPtr dwAttributes, IntPtr relativeSd, IntPtr bInheritHandle);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcAddUsersToFileEx(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr dwFlags, IntPtr reserved,
+            IntPtr fileName, IntPtr encryptionCertificates);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcFileKeyInfoEx(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr dwFileKeyInfoFlags, IntPtr reserved,
+            IntPtr fileName, IntPtr infoClass, IntPtr keyInfo);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcGetEncryptedFileMetadata(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr efsStreamBlob);
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr EfsRpcSetEncryptedFileMetadata(
+            IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr oldEfsStreamBlob,
+            IntPtr newEfsStreamBlob, IntPtr newEfsSignature);
+
+        internal static class X86
+        {
+            public static IntPtr EfsRpcOpenFileRaw(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr hContext, IntPtr fileName, IntPtr flags) =>
+                Invoke(stub, format, hBinding, hContext, fileName, flags);
+
+            public static IntPtr EfsRpcEncryptFileSrv(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName) =>
+                Invoke(stub, format, hBinding, fileName);
+
+            public static IntPtr EfsRpcDecryptFileSrv(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr openFlag) =>
+                Invoke(stub, format, hBinding, fileName, openFlag);
+
+            public static IntPtr EfsRpcQueryUsersOnFile(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr users) =>
+                Invoke(stub, format, hBinding, fileName, users);
+
+            public static IntPtr EfsRpcQueryRecoveryAgents(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr recoveryAgents) =>
+                Invoke(stub, format, hBinding, fileName, recoveryAgents);
+
+            public static IntPtr EfsRpcRemoveUsersFromFile(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr users) =>
+                Invoke(stub, format, hBinding, fileName, users);
+
+            public static IntPtr EfsRpcAddUsersToFile(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr encryptionCertificates) =>
+                Invoke(stub, format, hBinding, fileName, encryptionCertificates);
+
+            public static IntPtr EfsRpcFileKeyInfo(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr infoClass, IntPtr keyInfo) =>
+                Invoke(stub, format, hBinding, fileName, infoClass, keyInfo);
+
+            public static IntPtr EfsRpcDuplicateEncryptionInfoFile(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr srcFileName, IntPtr destFileName,
+                IntPtr dwCreationDisposition, IntPtr dwAttributes, IntPtr relativeSd, IntPtr bInheritHandle) =>
+                Invoke(stub, format, hBinding, srcFileName, destFileName, dwCreationDisposition, dwAttributes, relativeSd, bInheritHandle);
+
+            public static IntPtr EfsRpcAddUsersToFileEx(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr dwFlags, IntPtr reserved,
+                IntPtr fileName, IntPtr encryptionCertificates) =>
+                Invoke(stub, format, hBinding, dwFlags, reserved, fileName, encryptionCertificates);
+
+            public static IntPtr EfsRpcFileKeyInfoEx(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr dwFileKeyInfoFlags, IntPtr reserved,
+                IntPtr fileName, IntPtr infoClass, IntPtr keyInfo) =>
+                Invoke(stub, format, hBinding, dwFileKeyInfoFlags, reserved, fileName, infoClass, keyInfo);
+
+            public static IntPtr EfsRpcGetEncryptedFileMetadata(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr efsStreamBlob) =>
+                Invoke(stub, format, hBinding, fileName, efsStreamBlob);
+
+            public static IntPtr EfsRpcSetEncryptedFileMetadata(
+                IntPtr stub, IntPtr format, IntPtr hBinding, IntPtr fileName, IntPtr oldEfsStreamBlob,
+                IntPtr newEfsStreamBlob, IntPtr newEfsSignature) =>
+                Invoke(stub, format, hBinding, fileName, oldEfsStreamBlob, newEfsStreamBlob, newEfsSignature);
+
+            private static IntPtr Invoke(IntPtr stub, IntPtr format, params IntPtr[] args)
+            {
+                GCHandle argsHandle = GCHandle.Alloc(args, GCHandleType.Pinned);
+                try
+                {
+                    return rpcrt4.NdrClientCall2x86(stub, format, argsHandle.AddrOfPinnedObject());
+                }
+                finally
+                {
+                    argsHandle.Free();
+                }
+            }
+        }
+
+        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr NdrClientCall2x86(
             IntPtr pStubDesc, 
             IntPtr pProcFormat, 
             IntPtr pArgs
-        );
-
-        [DllImport("rpcrt4.dll", EntryPoint = "NdrClientCall2", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-        public static extern IntPtr NdrClientCall2x64(
-            IntPtr pStubDesc,
-            IntPtr pProcFormat,
-            IntPtr binding,
-            IntPtr phContext,
-            [MarshalAs(UnmanagedType.LPWStr)]
-            string fileName,
-            int flags
         );
 
         [DllImport("rpcrt4.dll", CharSet = CharSet.Unicode, SetLastError = true)]
